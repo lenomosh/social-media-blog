2e15d3a20166326c54ef34c5d958305943cb61be
Lennox <omondilenox@outlook.com>
2020-09-28 02:07:58 +0300

    fixed flask uploads error

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,11 @@ from flask_cors import CORS
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+
 photos = UploadSet('photos', IMAGES)
 jwt = JWTManager()
 migrate = Migrate()
+
 
 def create_app(config_name='dev'):
     app = Flask(__name__, instance_relative_config=True)
@@ -15,7 +17,7 @@ def create_app(config_name='dev'):
     app.config.from_object(config_options[config_name])
     db.init_app(app)
     jwt.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
     from app.api import api
     app.register_blueprint(api, url_prefix='/api')
     configure_uploads(app, photos)
