6141152d8ce871efed914ff6f3e170394a8c71c4
Lennox <omondilenox@outlook.com>
2020-09-28 13:19:50 +0300

    changed env to prod

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager
 from flask_migrate import MigrateCommand
-app = create_app('development')
+app = create_app('production')
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 if __name__ == '__main__':
