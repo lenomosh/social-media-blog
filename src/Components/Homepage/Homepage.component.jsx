7418ae8a0c2f49a03146fa4b5050fa415d20a8da
Lennox <omondilenox@outlook.com>
2020-09-30 18:41:09 +0300

    added directory structure for homepage

---
+++ src/Components/Homepage/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Homepage.component.scss'
+
+const Homepage =()=>{
+
+
+    return(
+        <div className="Homepage">
+
+        </div>
+    )
+}
+export default Homepage
